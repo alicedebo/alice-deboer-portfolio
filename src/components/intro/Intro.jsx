6fc0fc578016d157b0d37dd069193c5a24d44f1b
Allison Richardel <airichardel@gmail.com>
2021-11-13 09:38:35 -0700

    more intro work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,6 @@
 import "./intro.scss"
 import me from "../../images/alice.png";
+import arrow from "../../images/arrow.png"
 
 export default function Intro() {
     return (
@@ -10,14 +11,15 @@ export default function Intro() {
                 </div>
                 
             </div>
-            <span className="material-icons">south</span>
             <div className="right">
                 <div className="wrapper">
                     <h2>Hello, I'm</h2>
                     <h1>Alice Richardel-DeBoer</h1>
                     <h3>Full-Stack Developer<span></span></h3>
                 </div>
-                
+                <a href="#portfolio">
+                <img src={arrow} alt="arrow pointing down"/>
+                </a>
             </div>
             
         </div>
