305e2b67c30384e2fbaee80b235d817aa09f44ae
Allison Richardel <airichardel@gmail.com>
2021-11-20 08:52:36 -0700

    pix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
 import "./intro.scss"
 import me from "../../images/alice.png";
-import arrow from "../../images/arrow.png"
+import arrow from "../../images/arrow.png";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+const textRef = useRef();
+useEffect(() => {
+    init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500, 
+        backSpeed: 60,
+        strings: ["React", "JavaScript", "Node", "HTML5", "CSS", "MERN", "MySQL", "NoSql", "MongoDB", "Express", "ORM", "MVC", "OOP", "APIs"],
+    })
+},[]);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -14,8 +26,8 @@ export default function Intro() {
             <div className="right">
                 <div className="wrapper">
                     <h2>Hello, I'm</h2>
-                    <h1>Alice Richardel-DeBoer</h1>
-                    <h3>Full-Stack Developer<span></span></h3>
+                    <h1>Alice DeBoer</h1>
+                    <h3>Full-Stack Developer <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                 <img src={arrow} alt="arrow pointing down"/>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,80 @@
 import "./portfolio.scss"
+import one from "../../images/one.png";
+import two from "../../images/two.png";
+import three from "../../images/three.png";
+import four from "../../images/four.png";
+import five from "../../images/five.png";
+import six from "../../images/six.png";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useEffect, useState } from "react";
 
-export default function Contact() {
+export default function Portfolio() {
+    const [selected, setSelected] = useState("featured")
+    const list = [
+        {
+            id: "Recipe Generator",
+            title: "Recipe Generator",
+        },
+        {
+            id: "Note Taker",
+            title: "Note Taker",
+        },
+        {
+            id: "Team Profile Generator",
+            title: "Team Profile Generator",
+        },
+        {
+            id: "ReadMe Generator",
+            title: "ReadMe Generator",
+        },
+        {
+            id: "Weather Dashboard",
+            title: "Weather Dashboard",
+        },
+        {
+            id: "Coding Quiz",
+            title: "Coding Quiz",
+        }
+    ]
     return (
         <div className="portfolio" id="portfolio">
-            portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item)=>(
+                    <PortfolioList 
+                    title={item.title} 
+                    active={selected === item.id} 
+                    setSelected={setSelected}
+                    id={item.id}
+                    />
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src={one} alt="" />
+                    <h3>Recipe Generator</h3>
+                </div>
+                <div className="item">
+                    <img src={two} alt="" />
+                    <h3>Note Taker</h3>
+                </div>
+                <div className="item">
+                    <img src={three} alt="" />
+                    <h3>Team Profile Generator</h3>
+                </div>
+                <div className="item">
+                    <img src={four} alt="" />
+                    <h3>ReadMe Generator</h3>
+                </div>
+                <div className="item">
+                    <img src={five} alt="" />
+                    <h3>Weather Dashboard</h3>
+                </div>
+                <div className="item">
+                    <img src={six} alt="" />
+                    <h3>Coding Quiz</h3>
+                </div>
+            </div>
         </div>
     )
 }
