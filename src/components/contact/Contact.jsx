b859e2ba6fbf095e5d7f485209ea737f61da37b4
Allison Richardel <airichardel@gmail.com>
2021-11-26 20:59:40 -0700

    fini with code, work on readme

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
-import "./contact.scss"
+import "./contact.scss";
+import React, { useState } from 'react';
 
 export default function Contact() {
+    const [values, setValues] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+    });
+
+    const [submitted, setSubmitted] = useState(false);
+    const [valid, setValid] = useState(false);
+
+    const handleFirstNameInputChange = (event) => {
+        setValues({...values, firstName: event.target.value})
+    }
+    const handleLastNameInputChange = (event) => {
+        setValues({...values, lastName: event.target.value})
+    }
+    const handleEmailInputChange = (event) => {
+        setValues({...values, email: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(values.firstName && values.lastName && values.email) {
+            setValid(true);
+        }
+        setSubmitted(true)
+    }
     return (
         <div className="contact" id="contact">
-            contact
+            <h1>Want to collaborate? :)</h1>
+            <div className="form-container">
+                <form className="submit-form" onSubmit= {handleSubmit}>
+                    {submitted && valid ? <div className="success-message">Success, thank you for your interest!</div> : null} 
+                    <input onChange = {handleFirstNameInputChange} value= {values.firstName} className = "form-field" placeholder = "First Name" name="firstName"/>
+                    {submitted && !values.firstName ? <span>Please enter your first name</span>: null}
+                    <input onChange = {handleLastNameInputChange} value= {values.lastName} className = "form-field" placeholder = "Last Name" name="lastName"/> 
+                    {submitted && !values.lastName ? <span className="error">Please enter your last name</span>: null}
+                    <input onChange = {handleEmailInputChange} value= {values.email} className = "form-field" placeholder = "Email" name="email"/> 
+                    {submitted && !values.email ? <span>Please enter your email address.</span>: null}
+                    <button className = "form-field" type = "submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,68 @@
-import "./works.scss"
+import "./works.scss";
+import rightarrow from "../../images/right-arrow.png";
+import work from "../../images/work.png";
+import school from "../../images/school.png";
+import contact from "../../images/contact.png";
+import workicon from "../../images/workicon.png";
+import schoolicon from "../../images/schoolicon.png";
+import contacticon from "../../images/contacticon.png";
+import { useState  } from "react";
 
-export default function Contact() {
+export default function Works() {
+    const [currentSlide,setCurrentSlide] = useState(0);
+    const data = [
+        {
+            id: "1",
+            icon: workicon,
+            title: "Work Experience",
+            description: "",
+            img: work
+        },
+        {
+            id: "2",
+            icon: schoolicon,
+            title: "University of Denver",
+            description: "Coding Bootcamp Graduate 2021",
+            img: school
+        },
+        {
+            id: "3",
+            icon: contacticon,
+            title: "Contact Me",
+            description: "Information above or scroll once :)",
+            img: contact
+        }
+    ];
+
+    const handleClick = (way) =>{
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
+        setCurrentSlide(currentSlide<data.length -1 ?currentSlide+1 : 0)
+    }
     return (
-        <div className="works">
-            works
+        <div className="works" id="works">
+            <div className="slider" style={{ transform: `translateX(-${currentSlide *100}vw)` }}>
+                {data.map(d=>(
+
+                
+                    <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img className={"img"} src={d.icon} alt="" />
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>{d.description}</p>
+                            </div>
+                        </div>
+                        <div className="right">
+                           <img src={d.img} alt="" />
+                        </div>
+                    </div>
+                </div>))} 
+            </div>
+        <img src={rightarrow} className="arrow left" alt="change to a right arrow" onClick={()=>handleClick("left")} />
+        <img src={rightarrow} className="arrow right" alt="change to a right arrow" onClick={()=>handleClick()} />
         </div>
     )
 }
